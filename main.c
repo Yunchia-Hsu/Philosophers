6a384e61559a68c6011a153c660bc8fb6c987d87
Yun-Chia <127295359+Yunchia-Hsu@users.noreply.github.com>
2024-07-10 15:15:50 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,5 @@
+#include "philo.h"
+
+//valid input  The number of philosophers , The time a philosopher will die if he doesn’t eat , The time it takes a philosopher to eat , The time it takes a philosopher to sleep   > 0   philo < 200
+
+//structure
