0f33b004db7999163a60c6ff8b2f0c8fb532e008
Yun-Chia Hsu <yh24hsu@gmail.com>
2024-07-10 10:29:32 +0300

    commit test

--- main.c
+++ main.c
@@ -2,4 +2,7 @@
 
 //valid input  The number of philosophers , The time a philosopher will die if he doesn’t eat , The time it takes a philosopher to eat , The time it takes a philosopher to sleep   > 0   philo < 200
 
-//structure
+//every philo is a thread 
+
+// each philo geos into the routine function
+
---
+++ philo.h
@@ -0,0 +1,16 @@
+
+
+
+typedef struct s_philo
+{
+    pthread_t t;
+    int philo_n;
+
+} t_philo
+
+
+typedef struct s_program
+{
+    int *philo;
+
+} t_program
