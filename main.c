003be299b68a8efa6cd93c9b23a6d73b6a20a971
Yun-Chia Hsu <yh24hsu@gmail.com>
2024-07-10 13:16:02 +0300

    draft

--- main.c
+++ main.c
@@ -1,8 +1,34 @@
 #include "philo.h"
 
 //valid input  The number of philosophers , The time a philosopher will die if he doesn’t eat , The time it takes a philosopher to eat , The time it takes a philosopher to sleep   > 0   philo < 200
-
-//every philo is a thread 
+init_program
+init_philo
+//every philo is a thread  create threads; monitor is also a thread
 
 // each philo geos into the routine function
+// eat , sleep, think
+routine()
+{
+    //loop the routine, and break when dead flag is 1
+    // thinking: print message
+    //eating : ft_usleep , print message
+    //sleep :lock right / left forks , printf message , ft_usleep, drop forks (unlock fork)
 
+    //destroy mutex; free data if allocate any
+}
+
+monitor()
+{
+
+//infinite loop until philo dies
+//check die; time now - last meal < the time philo needs to die
+
+}
+
+
+/*
+log:
+X is thinking
+X is sleeping
+X is eating
+*/
--- philo.h
+++ philo.h
@@ -1,4 +1,20 @@
 
+#ifndef PHILO_H
+# define PHILO_H
+
+//write, usleep
+# include <unistd.h>
+//malloc, free
+#include <stdlib.h>
+//memset
+# include <string.h>
+//printf 
+# include <stdio.h>
+//pthread, mutex
+# include <pthread.h>
+//gettimeofday
+# include <sys/time.h>
+
 
 
 typedef struct s_philo
@@ -11,6 +27,17 @@ typedef struct s_philo
 
 typedef struct s_program
 {
+    int dead;
     int *philo;
-
+    //   3   pthread_mutex_t 
+    pthread_mutex_t fork;
 } t_program
+
+
+
+
+
+
+
+
+#endif
